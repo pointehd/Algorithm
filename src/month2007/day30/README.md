9b835a4f0a632068a0bea427afc44b2817ff5bb6
DONGHYUN HWANG <pointehd@gmail.com>
2020-08-31 22:55:11 +0900

    leetCode 121. Best Time to Buy and Sell Stock
    알고리즘 시작

---
+++ src/month2007/day30/README.md
@@ -0,0 +1,8 @@
+# leetCode 121. Best Time to Buy and Sell Stock
+
+! [문제] (../img/20200830.png)
+
+[문제링크](https://leetcode.com/problems/best-time-to-buy-and-sell-stock)
+
+> 느낀점
+> > 문제를 잘못 이해해서 조금 오래걸렸다... 영어공부도 해야겠다ㅠ
---
+++ src/month2007/day30/Souution1.java
@@ -0,0 +1,18 @@
+package month2007.day30;
+
+public class Souution1 {
+
+    public int maxProfit(int[] prices) {
+    	int minIndex = 0;
+        int maxProfit = 0;
+        
+        for(int i=0; i< prices.length; i++){
+            if ( prices[i] < prices[minIndex]) {
+            	minIndex = i;
+            } else {
+            	maxProfit = prices[i] - prices[minIndex] > maxProfit ? prices[i] - prices[minIndex] : maxProfit;
+            }
+        }
+        return maxProfit;
+    }
+}
