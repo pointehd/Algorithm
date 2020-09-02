94db85f9b492dd538cf53d014a2f5479ae5aaa40
DONGHYUN HWANG <pointehd@gmail.com>
2020-09-02 23:25:35 +0900

    알고리즘 문제풀이
    leet Code 543. Diameter of Binary Tree

--- src/month2009/day01/README.md
+++ src/month2009/day01/README.md
@@ -1,9 +1,19 @@
-# 543. Diameter of Binary Tree
+# leetCode 543. Diameter of Binary Tree
 
-![문제이미지]()
+![문제이미지](https://github.com/pointehd/Algorithm/blob/master/img/200002.png?raw=true)
 
 [문제링크](https://leetcode.com/problems/diameter-of-binary-tree/)
 
+## 메모리및 시간
+* 실행시간 :14 ms
+* 메모리 : 39.8 MB
+
+
 ## 느낀점
-> 와 너무 어렵다 풀고 자야겠어..
+> ~~와 너무 어렵다 풀고 자야겠어..~~ : 200901   
+> 진짜 나는 아직 멀었다... 너무 못푼거 같은데 아직도 어렵다 이런문제도있는 거지 ㅠㅠ   
+> 쉬운게 하나도 없다 정말ㅠㅠ   
+> 고맙다 대수야 너가 조언해준게 아니면 절대 못풀었을지도   
+> 분할정복 ?? 연습아자! 
+> 다른 방법이 있으면 다시 풀어보자 ! 
 
--- src/month2009/day01/Solution01.java
+++ src/month2009/day01/Solution01.java
@@ -1,5 +1,54 @@
 package month2009.day01;
 
 public class Solution01 {
-
+	
+	public int max;
+    public int diameterOfBinaryTree(TreeNode root) {
+        preOrder(root);
+        return max;
+    }
+    
+    public int nodeCount(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }else{
+            return 1 + Math.max(nodeCount(root.left), nodeCount(root.right));
+        }                           
+    }
+    public int getLength(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return nodeCount(root.left) + nodeCount(root.right);
+    }                     
+    
+    public void preOrder(TreeNode root){
+        if(root == null){
+            return ;
+        }
+        if(root.left != null)
+            preOrder(root.left);
+        if(root.right != null)
+            preOrder(root.right);
+        
+        int num = getLength(root);
+        if(max < num){
+            max = num;
+        }
+    }
+    static public class TreeNode {
+    	 int val;
+    	 TreeNode left;
+    	 TreeNode right;
+    	 TreeNode() {}
+    	 TreeNode(int val) { this.val = val; }
+    	 TreeNode(int val, TreeNode left, TreeNode right) {
+    		this.val = val;
+	 		this.left = left;
+	 		this.right = right;
+    	 }
+	 }
 }
