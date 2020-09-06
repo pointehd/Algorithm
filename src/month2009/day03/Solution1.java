555c4e9a7911406b122e8ea39b5d788a443d98e3
DONGHYUN HWANG <pointehd@gmail.com>
2020-09-06 21:32:16 +0900

    카카오 코딩연습
    문자열 압축

--- src/month2009/day03/Solution1.java
+++ src/month2009/day03/Solution1.java
@@ -1,5 +1,61 @@
 package month2009.day03;
 
 public class Solution1 {
-
+	
+	public static void main(String[] args) {
+		Solution1 sol = new Solution1();
+		String s= "aabbaccc";
+//		String s = "ababcdcdababcdcd";
+		int min = s.length();
+		
+		for(int i=1; i<s.length(); i++) {
+			int aa = sol.solution(s, i);
+			System.out.println(i+ ":" + aa);
+			if(min > aa) {
+				min = aa;
+			}
+		}
+		
+		System.out.println(min);
+	}
+	
+    public int solution(String s, int sliceCount){
+        int length = 0;
+        String shortStr = s.substring(0, sliceCount);
+        int count = 0;
+        while(true ){
+        	if(s.startsWith(shortStr)) {
+        		count++;
+        		try {
+        			s = s.substring(sliceCount, s.length());
+				} catch (Exception e) {
+					System.out.println("악 ");
+					if(count == 1) {
+	        			length += sliceCount;
+	        		}else {
+	        			length += sliceCount + (count+"").split("").length;	
+	        		}
+	        		return length;
+				}
+        	}else {
+        		
+        		if(count == 1) {
+        			length += sliceCount;
+        		}else {
+        			length += sliceCount + (count+"").split("").length;	
+        		}
+        		try {
+        			shortStr = s.substring(0, sliceCount);	
+				} catch (Exception e) {
+					System.out.println("length:"+length+" "+s+"\n");
+					length += s.length();
+					return length;
+				}
+        		count = 0;
+        		
+        	}
+        	
+        	
+        }
+    }
 }
