685228cdfdfb2dc7c67d9df889effc439d91a027
DONGHYUN HWANG <pointehd@gmail.com>
2020-09-07 21:52:28 +0900

    프로그래머스 기능개발
    옛날 문제와 비교 한 결과 별차이없음.

--- src/month2009/day07/Solution1.java
+++ src/month2009/day07/Solution1.java
@@ -1,13 +1,18 @@
 package month2009.day07;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution1 {
 	public static void main(String[] args) {
+		int[] progresses = {93, 30, 55};
+		int[] speeds = {1, 30, 5};
+		Solution1 sol = new Solution1();
+		sol.solution(progresses, speeds);
 		
 	}
-	
+	// 옛날에 푼 문제.
 	public int[] beforeSolution(int[] progresses, int[] speeds) {
 		int[] answer = {};
 		int size = progresses.length-1 ;
@@ -40,4 +45,36 @@ public class Solution1 {
 		}
 		return answer;
 	}
+	
+	// 20.09.07 푼 문제.
+	public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> result = new ArrayList<Integer>();
+        int startIndex = 0;
+        while (startIndex < progresses.length){
+            for(int i=startIndex; i<progresses.length; i++){
+                progresses[i] += speeds[i];
+            }
+            int delete = 0;
+            for(int i=startIndex; i<progresses.length; i++){
+                if (progresses[i] >= 100){
+                    delete++;
+                } else {
+                	if(delete >0) {
+                		startIndex += delete;
+                        result.add(delete);
+                	}
+                    break;
+                }
+                if(i==progresses.length-1) {
+                	startIndex += delete;
+                    result.add(delete);
+                }
+            }
+        }
+        int[] resultArray = new int[result.size()];
+        for(int i=0;i < result.size(); i++){
+            resultArray[i] = result.get(i);
+        }
+        return resultArray;
+    }
 }
