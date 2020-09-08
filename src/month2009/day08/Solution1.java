1fd671b5c310f576c0ea76dcd53e7412f07d930f
DONGHYUN HWANG <pointehd@gmail.com>
2020-09-08 22:34:02 +0900

    알고리즘
    프로그래머스 체육복

--- src/month2009/day08/Solution1.java
+++ src/month2009/day08/Solution1.java
@@ -1,14 +1,15 @@
 package month2009.day08;
 
-import java.util.Arrays;
-
 public class Solution1 {	
 	public static void main(String[] args) {
-		int[] array  = {};
+		int n = 6;
+		int[] lost = {1, 2, 6};
+		int[] reserve = {2, 3, 4, 5};
 		
-		Arrays.asList(array);
-		
-		
-		
+		System.out.println("노답 "+ solution(n, lost, reserve));
+	}
+	
+	public static int solution(int n, int[] lost, int[] reserve) {
+		return 0;
 	}
 }
