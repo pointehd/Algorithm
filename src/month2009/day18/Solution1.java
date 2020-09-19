d4e3df815bae2fda34e8ef780eddb384973720c4
Donghyun Hwang <pointehd@naver.com>
2020-09-19 18:16:16 +0900

    주석 추가

--- src/month2009/day18/Solution1.java
+++ src/month2009/day18/Solution1.java
@@ -1,7 +1,14 @@
 package month2009.day18;
 
 public class Solution1 {
-	
+	/**
+	 * <pre>
+	 *  최상단 시간 값이 이전 (시간+오른쪽카운터)값
+	 *  (1,3) / (4,6) / (10,12) / (22,24) / (46,48)
+	 *  두개를 더한게 다음 최상단의 시간값
+	 *  오른쪽 카운트넌 시간 + 2
+	 * </pre>
+	 * */
 	private static long strangeCounter(long t ) {
 		long n =1;
 		while (t >= n + n + 2) {
