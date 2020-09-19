37d8b9730658991bb6186ff26377dc148cade429
Donghyun Hwang <pointehd@naver.com>
2020-09-19 13:51:56 +0900

    알고리즘
    Strange Counter

--- src/month2009/day18/Solution1.java
+++ src/month2009/day18/Solution1.java
@@ -2,5 +2,12 @@ package month2009.day18;
 
 public class Solution1 {
 	
-	
+	private static long strangeCounter(long t ) {
+		long n =1;
+		while (t >= n + n + 2) {
+			n += n + 2;
+			
+		}
+		return n+n+2-t;
+	}
 }
