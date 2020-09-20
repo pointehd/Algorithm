dfc2e08be64800ee2d582e4ddd93fd3f46b11bc9
Donghyun Hwang <pointehd@naver.com>
2020-09-20 16:14:04 +0900

    Leet code
     Maximum Depth of Binary Tree

--- src/month2009/week3/MaximumDepthOfBinaryTree.java
+++ src/month2009/week3/MaximumDepthOfBinaryTree.java
@@ -1,5 +1,58 @@
 package month2009.week3;
 
 public class MaximumDepthOfBinaryTree {
-
+	
+	/**
+	 * RunTime : 0 ms
+	 * Memory : 39.5 MB
+	 * 
+	 * @date 2020. 09. 20
+	 * */
+	public int maxDepth(TreeNode root) {
+		if(root == null) {
+			return 0;
+		}
+		if (root.left != null && root.right !=null) {
+			return Math.max(maxDepth(root.left), maxDepth(root.right)) +1; 
+		} else if (root.left == null && root.right != null) {
+			return maxDepth(root.right) +1;
+		} else if (root.left != null && root.right == null) {
+			return maxDepth(root.left)+1; 
+		} else {
+			return 1;
+		}
+		
+    }
+	
+	/**
+	 * RunTime : 0 ms
+	 * Memory : 39.5 MB
+	 * 
+	 * @date 2020. 09. 01
+	 * */
+	public int nodeCount(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }else{
+            return 1 + Math.max(nodeCount(root.left), nodeCount(root.right));
+        }                           
+    }
+	
+	
+	
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
 }
--- src/month2009/week3/README.md
+++ src/month2009/week3/README.md
@@ -3,27 +3,27 @@
 * 이미지는 용량상의 문제로 올리지 않겠습니다.
 * 
 
-## Maximum Depth of Binary Tree
+## LeetCode. Maximum Depth of Binary Tree
 
 [문제링크](https://leetcode.com/problems/maximum-depth-of-binary-tree/)
 
 ### 메모리및 시간
-* 실행시간 : 없음 
-* 메모리 : 없음 
+* 실행시간 : 0ms
+* 메모리 : 39.5 MB
 
 
 ### 느낀점
->
+> 이전에 풀었던 문제와 유사한 문제여서 금방풀기는 했지만 나는 퇴화하는게 확실하다 ㅋ.ㅋ 
 
 ### 코드개선 
 
 
 ### 참고
-* 
+* [내가 앞전에 푼 문제](https://github.com/pointehd/Algorithm/tree/master/src/month2009/day01)
 
 ---
 
-## Merge Two Sorted Lists
+## LeetCode. Merge Two Sorted Lists
 
 [문제링크](https://leetcode.com/problems/merge-two-sorted-lists/)
 
