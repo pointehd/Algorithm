689e1a185bb59d0f89d92551798d52a50c0e4c4d
Donghyun Hwang <pointehd@naver.com>
2020-10-14 23:09:24 +0900

    문제 풀이

--- src/month2010/week3/MutiplyStrings.java
+++ src/month2010/week3/MutiplyStrings.java
@@ -7,6 +7,8 @@ public class MutiplyStrings {
 	/**
 	 * 실행시간 : 11 ms
 	 * 메모리 : 39.5 MB
+	 * 
+	 * BigInteger 사용불가 라고써져있다.
 	 * @date 2020. 10. 12
 	 * */
 	public static String mutiply(String num1, String num2) {
