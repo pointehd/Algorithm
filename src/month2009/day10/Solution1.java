b93c38d5dd9ae7921e324d970edc714de5303b9c
Donghyun Hwang <pointehd@naver.com>
2020-10-21 23:52:28 +0900

    ...

--- src/month2009/day10/Solution1.java
+++ src/month2009/day10/Solution1.java
@@ -6,8 +6,20 @@ public class Solution1 {
 	}
 	
 	public static int[] coloringBook(int m, int n, int[][] picture) {
-		int[] result = null;
-		
-		return result;
+		int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+        
+        
+        
+        
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+	}
+	
+	public static int check(int x, int n, int[][] picture) {
+		int max = 0;
+		return check(max, n, picture) +1;
 	}
 }
