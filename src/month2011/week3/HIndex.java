1f28f258c98dffabbe3bc1b61e88edee3e53979b
Donghyun Hwang <pointehd@naver.com>
2020-11-16 22:50:10 +0900

    문제풀이 Programmers. H-Index

--- src/month2011/week3/HIndex.java
+++ src/month2011/week3/HIndex.java
@@ -1,32 +1,24 @@
 package month2011.week3;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class HIndex {
 	public static void main(String[] args) {
 		HIndex hIndex = new HIndex();
 		int[] citations = {3,0,6,1,5};
-		hIndex.hIndex(citations);
+		System.out.println(hIndex.hIndex(citations));
 	}
 	
 	public int hIndex(int[] citations) {
-		int result = 0;
+		Arrays.sort(citations);
 		
-		List<Integer> list = new ArrayList<Integer>();
-		for(int num : citations) {
-			list.add(num);
-		}
-		
-		Collections.sort(list);
-		
-		for (int i=0; i< list.size(); i++) {
-			if(list.size() - i <  list.get(i)) {
-				return list.get(i-1);
+		for (int i=0; i< citations.length; i++) {
+			int result = citations.length - i;
+			
+			if(citations[i] >= result) {
+				return result;
 			}
 		}
-		return result;
+		return 0;
 	}
 }
