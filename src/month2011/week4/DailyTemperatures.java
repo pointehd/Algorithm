a271905d4255a0f09cf5eccaaa95a8dcfa8a34aa
Donghyun Hwang <pointehd@naver.com>
2020-11-28 20:00:47 +0900

    LeetCode. 739. Daily Temperatures

--- src/month2011/week4/DailyTemperatures.java
+++ src/month2011/week4/DailyTemperatures.java
@@ -1,12 +1,20 @@
 package month2011.week4;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class DailyTemperatures {
-	
+	/**
+	 * 쉽군!
+	 * */
 	public int[] dailyTemperatures(int[] T) {
-		int[] result = null;
+		int[] result = new int [T.length];
+		for(int i=0; i< T.length-1; i++) {
+			for(int j=i+1; j< T.length; j++) {
+				if(T[i] < T[j]) {
+					result[i] = j-i;
+					break;
+				}
+			}
+		}
 		return result;
 	}
 }
