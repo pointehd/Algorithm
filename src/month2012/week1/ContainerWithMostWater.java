3b6783db8667c3645975bb8a547bc7136ae1ea61
Donghyun Hwang <pointehd@naver.com>
2020-12-05 16:45:34 +0900

    LeetCode. 11. Container With Most Water

--- src/month2012/week1/ContainerWithMostWater.java
+++ src/month2012/week1/ContainerWithMostWater.java
@@ -3,7 +3,19 @@ package month2012.week1;
 public class ContainerWithMostWater {
 	
 	public int containerWithMostWater(int[] height) {
-		return 0;
+		int result = 0;
+		int leftIndex = 0;
+		int rightIndex = height.length-1;
+		while(leftIndex < rightIndex) {
+			result = Math.max(result, 
+					Math.min(height[leftIndex], height[rightIndex]) * (rightIndex -leftIndex)); 
+			if(height[leftIndex] <= height[rightIndex]) {
+				leftIndex ++;
+			}else {
+				rightIndex --;
+			}
+		}
+		return result;
 	}
 
 }
