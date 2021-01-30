87fa23c2023c1181cff8b8ebb3301a7794028b3e
Donghyun Hwang <pointehd@naver.com>
2021-01-30 15:08:34 +0900

    LeetCode 347. Top K Frequent Elements
    https://leetcode.com/problems/top-k-frequent-elements/

--- src/year2021/leetCode/README.md
+++ src/year2021/leetCode/README.md
@@ -3,9 +3,10 @@
 ### 완료문제 
 * [Decode String](https://leetcode.com/problems/decode-string/) - [코드](https://github.com/pointehd/Algorithm/blob/master/src/year2021/leetCode/DecodeString.java)
 * [Rotate Image](https://leetcode.com/problems/rotate-image/) - [코드](https://github.com/pointehd/Algorithm/blob/master/src/year2021/leetCode/RotateImage.java)
+* [Rotate Image](https://leetcode.com/problems/rotate-image/) - [코드](https://github.com/pointehd/Algorithm/blob/master/src/year2021/leetCode/TopKFrequentElements.java)
 
 ### 푸는 중인 문제 
-* [Rotate Image](https://leetcode.com/problems/rotate-image/) 
+ 
 
 ### 보류 문제
 
--- src/year2021/leetCode/TopKFrequentElements.java
+++ src/year2021/leetCode/TopKFrequentElements.java
@@ -1,2 +1,45 @@
-package year2021.leetCode;public class TopKFrequentElements {
+package year2021.leetCode;
+
+import java.util.*;
+
+/**
+ *
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ *
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ *
+ * 문제설명 : 배열이 주어지고 k개의 가장 빈번한 숫자 찾기
+ * */
+public class TopKFrequentElements {
+    /**
+     * Runtime : 11ms
+     * Memory Usage : 41.7MB
+     * */
+    public int[] topKFrequentElements(int[] nums, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums){
+            if (map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        List<Map.Entry<Integer, Integer>> listEntries = new ArrayList<>(map.entrySet());
+
+        Collections.sort(listEntries, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                // 오름차순
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        int[] result = new int[k];
+        for(int i=0; i< k; i++){
+            result[i] = listEntries.get(i).getKey();
+        }
+        return result;
+    }
 }
