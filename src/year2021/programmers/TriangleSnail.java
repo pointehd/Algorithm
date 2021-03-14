6ca44bea0134e75e1ab936b14474849018dcafac
Donghyun Hwang <pointehd@naver.com>
2021-03-14 12:31:25 +0900

    삼각달팽이 실행시간 및 메모리

--- src/year2021/programmers/TriangleSnail.java
+++ src/year2021/programmers/TriangleSnail.java
@@ -1,6 +1,17 @@
 package year2021.programmers;
 
 public class TriangleSnail {
+    /**
+     * 테스트 1 〉	통과 (0.01ms, 52.6MB)
+     * 테스트 2 〉	통과 (0.02ms, 52.4MB)
+     * 테스트 3 〉	통과 (0.02ms, 52.3MB)
+     * 테스트 4 〉	통과 (0.20ms, 54.7MB)
+     * 테스트 5 〉	통과 (0.21ms, 52.9MB)
+     * 테스트 6 〉	통과 (0.22ms, 53.3MB)
+     * 테스트 7 〉	통과 (7.51ms, 112MB)
+     * 테스트 8 〉	통과 (7.63ms, 112MB)
+     * 테스트 9 〉	통과 (7.70ms, 110MB)
+     * */
     public int[] solution(int n){
         int[] answer = new int[n*(n+1)/2];
         int val = 1, idx= 0, cnt =0;
