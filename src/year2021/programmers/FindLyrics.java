2765c9b44520841a3d937e2dfdaef12a05a61632
Donghyun Hwang <pointehd@naver.com>
2021-03-28 18:47:01 +0900

    Programmers. 가사 검색
    효율성 1,2,3 실패

--- src/year2021/programmers/FindLyrics.java
+++ src/year2021/programmers/FindLyrics.java
@@ -16,8 +16,22 @@ public class FindLyrics {
 
     public int[] solution(String[] words, String[] queries){
         return Arrays.stream(queries).mapToInt(q->{
-            String regix = "^"+q.replaceAll("\\?", "\\\\w{1}")+"$";
-            return (int)Arrays.stream(words).filter(w->Pattern.matches(regix, w)).count();
+//            String regix = "^"+q.replaceAll("\\?", "\\\\w{1}")+"$";
+            String type = q.charAt(0) != '?' ? "prefix" : q.charAt(q.length()-1) != '?' ? "postfix":"all";
+            return (int)Arrays.stream(words).filter(w->{
+                if(w.length() != q.length()){
+                    return false;
+                }
+                String regix = q.replaceAll("\\?", "");
+                switch (type){
+                    case "prefix":
+                        return w.startsWith(regix);
+                    case "postfix":
+                        return w.endsWith(regix);
+                    default:
+                        return w.length() == q.length();
+                }
+            }).count();
         }).toArray();
     }
 }
