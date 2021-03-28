bfa4d2934d75bc2a0095b33eed4b0b2401888f32
Donghyun Hwang <pointehd@naver.com>
2021-03-28 17:47:12 +0900

    Programmers. 가사검색 ing
    https://programmers.co.kr/learn/courses/30/lessons/60060

---
+++ src/year2021/programmers/FindLyrics.java
@@ -0,0 +1,25 @@
+package year2021.programmers;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class FindLyrics {
+    public static void main(String[] args) {
+        String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
+        String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
+
+        System.out.println(Arrays.stream(new FindLyrics().solution(words, queries)).boxed()
+                .map(i->i+"").collect(Collectors.joining(", ")));
+
+    }
+
+    public int[] solution(String[] words, String[] queries){
+        return Arrays.stream(queries).mapToInt(q->{
+            String regix = "^"+q.replaceAll("\\?", "\\\\w{1}")+"$";
+            return (int)Arrays.stream(words).filter(w->{
+                return Pattern.matches(regix, w);
+            }).count();
+        }).toArray();
+    }
+}
