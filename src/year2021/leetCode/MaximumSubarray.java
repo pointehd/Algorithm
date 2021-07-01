879d17d0b2db146b95fc3439fb4df47fbe29f22e
Donghyun Hwang <pointehd@naver.com>
2021-07-01 23:29:51 +0900

    LeetCode 53. Maximum Subarray

--- src/year2021/leetCode/MaximumSubarray.java
+++ src/year2021/leetCode/MaximumSubarray.java
@@ -3,11 +3,24 @@ package year2021.leetCode;
 public class MaximumSubarray {
     /**
      * https://leetcode.com/problems/maximum-subarray/
-     *
+     * RunTime: 127 ms
+     * Memory: 38.8 MB
      * */
     public int maxSubArray(int[] nums){
+        int max = nums[0];
+        for (int i=0; i<nums.length; i++) {
+            int sum=nums[i];
+            max = Math.max(max, sum);
+            for (int j=i+1; j<nums.length; j++) {
+                sum += nums[j];
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
 
-
-        return 0;
+    public static void main(String[] args) {
+        int[] array = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(new MaximumSubarray().maxSubArray(array));
     }
 }
