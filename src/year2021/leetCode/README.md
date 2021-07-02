8cdd8af3822f98e23aa0c2e1d606d728902eba52
Donghyun Hwang <pointehd@naver.com>
2021-07-02 21:05:15 +0900

    LeetCode 78. Subsets
    https://leetcode.com/problems/subsets/

--- src/year2021/leetCode/README.md
+++ src/year2021/leetCode/README.md
@@ -8,9 +8,9 @@
 * [Target Sum](https://leetcode.com/problems/target-sum/) - [코드](https://github.com/pointehd/Algorithm/blob/master/src/year2021/leetCode/TargetSum.java)
 * [22.Generate Parentheses](https://leetcode.com/problems/generate-parentheses/) -[코드](https://github.com/pointehd/Algorithm/blob/master/src/year2021/leetCode/GenerateParentheses.java)
 * [53. Maximum Subarray](https://leetcode.com/problems/maximum-subarray/) - [코드](https://github.com/pointehd/Algorithm/blob/879d17d0b2db146b95fc3439fb4df47fbe29f22e/src/year2021/leetCode/MaximumSubarray.java)
+* [78. Subsets](https://leetcode.com/problems/subsets/) - [코드](https://github.com/pointehd/Algorithm/blob/879d17d0b2db146b95fc3439fb4df47fbe29f22e/src/year2021/leetCode/Subsets.java)
 
 ### 푸는 중인 문제 
-
 
 
 ### 보류 문제
--- src/year2021/leetCode/Subsets.java
+++ src/year2021/leetCode/Subsets.java
@@ -1,10 +1,43 @@
 package year2021.leetCode;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Subsets {
 
-    public List<List<Integer>> subsets(int[] nums){
-        return null;
+    List<List<Integer>> result = null;
+
+    /**
+     * LeetCode 78. Subsets
+     * https://leetcode.com/problems/subsets/
+     *
+     * RunTime: 1 ms
+     * Memory: 39.1 MB
+     * */
+    public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<>();
+        addList(0, nums, new ArrayList<>());
+        return result;
+    }
+
+    private void addList(int index, int[] nums, List<Integer> list) {
+        result.add(new ArrayList<>(list));
+        for (int i = index; i < nums.length; i++) {
+            list.add(nums[i]);
+            addList(i + 1, nums, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        new Subsets().subsets(nums) // 함수 호출
+                .stream().forEach(l -> { // 출력: 결과확인
+            System.out.print("[");
+            l.stream().map(e -> e + " ").forEach(System.out::print);
+            System.out.println("]");
+        });
     }
 }
