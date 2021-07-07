73de32d1a0ce7eaab30f95e7ca1ceb429f3723e0
Donghyun Hwang <pointehd@naver.com>
2021-07-07 21:35:17 +0900

    CombinationSum ing...

--- src/year2021/leetCode/CombinationSum.java
+++ src/year2021/leetCode/CombinationSum.java
@@ -1,4 +1,29 @@
 package year2021.leetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CombinationSum {
+    List<List<Integer>> result = null;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+
+
+        return result;
+    }
+
+    public void candidatesTracking(int[] candidates, int index, int tempSize, int target, List<Integer> tmp){
+        if (target == 0) {
+            result.add(new ArrayList(tmp));
+            return ;
+        }
+        for (int i=index; i<tmp.size(); i++) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
