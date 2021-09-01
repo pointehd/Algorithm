6676702b6e7fe11c73f60cda4d12b1accccf7bde
Donghyun Hwang <pointehd@naver.com>
2021-09-01 23:16:50 +0900

    programmers. 부족한 금액 계산하기
    https://programmers.co.kr/learn/courses/30/lessons/82612

---
+++ src/year2021/programmers/weekly/_8_1부족한금액계산하기.java
@@ -0,0 +1,20 @@
+package year2021.programmers.weekly;
+
+/**
+ * Programmers. 부족한 금액 계산하기
+ * https://programmers.co.kr/learn/courses/30/lessons/82612
+ *
+ * */
+public class _8_1부족한금액계산하기 {
+
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
