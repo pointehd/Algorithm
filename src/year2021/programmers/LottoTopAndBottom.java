2843bfd4fd4c8f3fd2cf398b250abbc0ca566796
donghyun <pointehd@gmail.com>
2021-12-23 00:05:50 +0900

    시간 수정 코드 리팩토링 필요

--- src/year2021/programmers/LottoTopAndBottom.java
+++ src/year2021/programmers/LottoTopAndBottom.java
@@ -8,6 +8,21 @@ import java.util.stream.Collectors;
  * Programmers. 로또의 최고 순위와 최저순위
  * https://programmers.co.kr/learn/courses/30/lessons/77484
  *
+ * 테스트 1 〉	통과 (2.26ms, 77.4MB)
+ * 테스트 2 〉	통과 (2.44ms, 77.6MB)
+ * 테스트 3 〉	통과 (2.70ms, 78.9MB)
+ * 테스트 4 〉	통과 (2.84ms, 76.3MB)
+ * 테스트 5 〉	통과 (2.68ms, 77.1MB)
+ * 테스트 6 〉	통과 (2.37ms, 72.1MB)
+ * 테스트 7 〉	통과 (2.73ms, 76.1MB)
+ * 테스트 8 〉	통과 (2.35ms, 79.2MB)
+ * 테스트 9 〉	통과 (2.81ms, 77MB)
+ * 테스트 10 〉	통과 (3.53ms, 78.3MB)
+ * 테스트 11 〉	통과 (2.54ms, 72.4MB)
+ * 테스트 12 〉	통과 (3.86ms, 78.4MB)
+ * 테스트 13 〉	통과 (2.50ms, 74.5MB)
+ * 테스트 14 〉	통과 (3.42ms, 79.6MB)
+ * 테스트 15 〉	통과 (2.45ms, 71MB)
  * */
 public class LottoTopAndBottom {
 
