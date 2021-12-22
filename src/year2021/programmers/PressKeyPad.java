f6c65d4790ea206a09a247b26c5da2972dff52f0
donghyun <pointehd@gmail.com>
2021-12-22 23:17:34 +0900

    Programmers. 키패드누르기 https://programmers.co.kr/learn/courses/30/lessons/67256

---
+++ src/year2021/programmers/PressKeyPad.java
@@ -0,0 +1,14 @@
+package year2021.programmers;
+
+/**
+ * Programmers. 키패드 누르기
+ * https://programmers.co.kr/learn/courses/30/lessons/67256
+ *
+ * 
+ * */
+public class PressKeyPad {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        return answer;
+    }
+}
