0e54b22f9f7c7817dcfb19d315abbde5563540f7
donghyun <pointehd@gmail.com>
2022-01-19 22:37:40 +0900

    Programmers 짝지어 제거하기 (미)
    https://programmers.co.kr/learn/courses/30/lessons/12973

--- src/year2021/programmers/_짝지어_제거하기.java
+++ src/year2021/programmers/_짝지어_제거하기.java
@@ -1,5 +1,24 @@
 package year2021.programmers;
 
-public class _2017_팁스다운 {
-    public 
+public class _짝지어_제거하기 {
+    public int solution(String s) {
+        StringBuffer sb = new StringBuffer(s);
+
+        int index = 0;
+        while (index < sb.length()) {
+            char tmp = sb.charAt(index);
+            if (tmp == sb.charAt(index + 1)) {
+                String tt = sb.toString();
+                sb.delete(index, index + 2);
+                System.out.println(tt+" : "+sb);
+                index =0;
+            }
+            index++;
+        }
+        return sb.length() == 0 ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new _짝지어_제거하기().solution("cdcd"));
+    }
 }
