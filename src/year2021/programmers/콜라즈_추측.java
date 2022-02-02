a5659d8bb1f2a947b0a8bf52057ba648a8b8ffdd
황동현 <pointehd@naver.com>
2022-02-02 21:52:53 +0900

    Update 콜라즈_추측.java
    Programmers. 콜라츠 추측 https://programmers.co.kr/learn/courses/30/lessons/12943

--- src/year2021/programmers/콜라즈_추측.java
+++ src/year2021/programmers/콜라즈_추측.java
@@ -1,37 +1,30 @@
 package year2021.programmers;
 
 /**
- * Programmers. 콜라츠 추측 TODO
+ * Programmers. 콜라츠 추측
  * https://programmers.co.kr/learn/courses/30/lessons/12943
- *
- * */
+ */
 public class 콜라즈_추측 {
 
     public int solution(int num) {
         int result = 0;
-        if (checkInputNum(num)){
-            return -1;
-        }
         long longNum = num;
-        do{
+        while (longNum != 1 && ++result < 500) {
             if (checkEvenNumber(longNum)) {
                 longNum /= 2;
             } else {
-                longNum =  3 * longNum + 1;
+                longNum = 3 * longNum + 1;
             }
-        }while (longNum != 1 && ++result < 500);
-        return result > 500 ? -1 : result;
+        }
+        return result >= 500 ? -1 : result;
     }
 
     public boolean checkEvenNumber(long num) {
         return num % 2 == 0;
     }
 
-    public boolean checkInputNum(int num){
-        return !(num > 1 && num < 8000000);
-    }
 
     public static void main(String[] args) {
-        System.out.println(new 콜라즈_추측().solution(626331));
+        System.out.println(new 콜라즈_추측().solution(1));
     }
 }
