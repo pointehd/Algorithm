82b5c4d35fe2bfc64c486d5e286226ce36515be2
황동현 <pointehd@naver.com>
2022-02-02 22:37:20 +0900

    큰 수 만들기 시간 작성
    큰 수만들기 시간변경

--- src/year2021/programmers/_큰_수_만들기.java
+++ src/year2021/programmers/_큰_수_만들기.java
@@ -5,6 +5,19 @@ import java.util.Stack;
 /**
  * Programmers. 큰 수 만들기
  * https://programmers.co.kr/learn/courses/30/lessons/42883
+ *
+ * 테스트 1 〉	통과 (0.25ms, 73.7MB)
+ * 테스트 2 〉	통과 (0.25ms, 74.3MB)
+ * 테스트 3 〉	통과 (0.33ms, 66.7MB)
+ * 테스트 4 〉	통과 (2.91ms, 77MB)
+ * 테스트 5 〉	통과 (1.23ms, 72.9MB)
+ * 테스트 6 〉	통과 (5.92ms, 73.1MB)
+ * 테스트 7 〉	통과 (14.60ms, 80.2MB)
+ * 테스트 8 〉	통과 (17.78ms, 91.5MB)
+ * 테스트 9 〉	통과 (32.12ms, 85.3MB)
+ * 테스트 10 〉	통과 (51.94ms, 101MB)
+ * 테스트 11 〉	통과 (0.18ms, 76.6MB)
+ * 테스트 12 〉	통과 (0.21ms, 72.1MB)
  * */
 class _큰_수_만들기 {
     public String solution(String numbers, int k) {
