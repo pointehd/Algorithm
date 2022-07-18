df144e01c267e50f0ffd23fe7069ebb128178a28
pointehd <pointehd@naver.com>
2022-07-18 22:21:14 +0900

    백준: 큐2
    https://www.acmicpc.net/problem/18258

--- src/year2022/baekjoon/_18258.java
+++ src/year2022/baekjoon/_18258.java
@@ -3,88 +3,66 @@ package year2022.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * BaekJoon
  * https://www.acmicpc.net/problem/18258
  * <p>
- * RunTime: 시간초과
- * Memory:
+ * RunTime: 37,9836 KB
+ * Memory: 1268 msㅎ
+ *
+ * 이게 이렇게 어렵네
  */
 class _18258 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int methodCount = Integer.parseInt(br.readLine());
-        Queue queue = new Queue();
-        for(int i=0; i< methodCount; i++){
-            String method = br.readLine();
-            queue.run(method);
+        Deque<String> deque = new LinkedList<>();
+        StringTokenizer stringTokenizer;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < methodCount; i++) {
+            stringTokenizer = new StringTokenizer(br.readLine(), " ");
+            switch (stringTokenizer.nextToken()) {
+                case "push":
+                    deque.push(stringTokenizer.nextToken());
+                    break;
+                case "pop":
+                    if(deque.isEmpty()) {
+                        stringBuilder.append("-1\n");
+                    }else {
+                        stringBuilder.append(deque.removeLast()).append("\n");
+                    }
+                    break;
+                case "size":
+                    stringBuilder.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    if(deque.isEmpty()) {
+                        stringBuilder.append("1\n");
+                    }else {
+                        stringBuilder.append("0\n");
+                    }
+                    break;
+                case "front":
+                    if(deque.isEmpty()) {
+                        stringBuilder.append("-1\n");
+                    }else {
+                        stringBuilder.append(deque.getLast()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if(deque.isEmpty()) {
+                        stringBuilder.append("-1\n");
+                    }else {
+                        stringBuilder.append(deque.getFirst()).append("\n");
+                    }
+                    break;
+            }
         }
+        System.out.println(stringBuilder.toString());
         br.close();
     }
 
-    static class Queue {
-        private List<Integer> list;
-        int back;
-
-        public Queue() {
-            this.list = new ArrayList<>();
-            back = -1;
-        }
-
-        private void push(int num) {
-            list.add(num);
-            back++;
-        }
-
-        private int pop() {
-
-            return list.isEmpty() ? -1:  list.remove(0);
-        }
-
-        private int size() {
-            return list.size();
-        }
-
-        private int empty() {
-            return list.isEmpty() ? 1 : 0;
-        }
-
-        private int front() {
-            return list.isEmpty() ? -1 : list.get(0);
-        }
-
-        private int back() {
-            return list.isEmpty() ? -1 : list.get(list.size() - 1);
-        }
-
-        public void run(String method){
-            String[] methods= method.split(" ");
-            String result = null;
-            switch (methods[0]) {
-                case "push":
-                    push(Integer.parseInt(methods[1]));
-                    break;
-                case "pop":
-                    System.out.println(pop());
-                    break;
-                case "size":
-                    System.out.println(size());
-                    break;
-                case "empty":
-                    System.out.println(empty());
-                    break;
-                case "front":
-                    System.out.println(front());
-                    break;
-                case "back":
-                    System.out.println(back());
-                    break;
-                default:
-            }
-        }
-    }
 }
